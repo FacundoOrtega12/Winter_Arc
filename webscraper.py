63421f7c39e48d38442f6e636bdf17e3ba485c85
Michael Wickenden <michael.wickenden19@gmail.com>
2024-09-01 13:53:10 -0700

    look

--- webscraper.py
+++ webscraper.py
@@ -1,7 +1,9 @@
 import urllib.request
+import re
 
 # URL of the web page to fetch
-url = 'https://en.wikipedia.org/wiki/Chris_Bumstead'
+url = 'https://www.linkedin.com/jobs/search/?currentJobId=3980097591&distance=25&geoId=103918656&keywords=Biotechnology&origin=JOBS_HOME_KEYWORD_HISTORY&refresh=true'
+
 
 try:
     # Open the URL and read its content
@@ -12,9 +14,16 @@ try:
     
     # Decode the data (if it's in bytes) to a string
     html_content = data.decode('utf-8')
-    
+
+    #remove HTML tags from page to clean it up :)
+    CLEANR = re.compile('<.*?>')
+    clean_html_content = re.sub(CLEANR, '', html_content) 
+    more_clean = clean_html_content.split()
+    #for word in more_clean:
+        #print(word)
+
     # Print the HTML content of the web page
-    print(html_content)
+    print(clean_html_content)
 
 except Exception as e:
     print("Error fetching URL:", e)
---
+++ winter_arc_2_even_arcier!.py
@@ -0,0 +1,6 @@
+WEBSCRAPER
+
+#While its running read urls as HTML file
+    #if words/content we want is in the file store the raw url
+        #move on
+    #move on
