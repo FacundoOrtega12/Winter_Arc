c681f6a52fb9833c5a4476b90f3821426022bc14
Michael <michael.wickenden19@gmail.com>
2024-09-17 20:23:22 -0700

    Many changes

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,9 @@
 import urllib.request
 import re
+from bs4 import BeautifulSoup
 
 # URL of the web page to fetch
-url = 'https://www.linkedin.com/jobs/search/?currentJobId=3980097591&distance=25&geoId=103918656&keywords=Biotechnology&origin=JOBS_HOME_KEYWORD_HISTORY&refresh=true'
-
+url = 'https://www.linkedin.com/jobs/search/?currentJobId=4011191575&distance=25&geoId=103918656&keywords=biotechnology&origin=JOBS_HOME_KEYWORD_HISTORY&refresh=true'
 
 try:
     # Open the URL and read its content
@@ -20,12 +20,21 @@ try:
     clean_html_content = re.sub(CLEANR, '', html_content) 
     more_clean = clean_html_content.split()
     test_clean = html_content.split()
-    f = open("test.txt", "w")
+    f = open("test.html", "w")  #THOUGHTS - need to store it as a base html file with no edits made to it that way hopefully it will search propery
+                                # or find a way I can use BS to search the text file
     for word in test_clean:
-        f.write(f"\n{word}")
+        for char in word:
+            f.write(char)
     f.close()
 
     # Print the HTML content of the web page
 
+    # Using BS we can pull out the html elements we want. Then we can make a Dic of link and time posted (idk if we need the time but want to factor it in search)
+    # use this list or dic of links to then serach through those for our jobs
+    with open("test.html") as m:
+        soup = BeautifulSoup(m.read(), 'html.parser')
+    soupy_txt = soup.find_all("div", class_= "job-search-card__listdate")
+    print("HERE", soupy_txt)
+    
 except Exception as e:
     print("Error fetching URL:", e)
