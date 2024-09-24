3f292778d5b61733720c763b0fe4db506dfb0912
Michael <michael.wickenden19@gmail.com>
2024-09-23 20:10:19 -0700

    MASSIVE CHANGES AND DIRECTION

---
+++ TO_DO.py
@@ -0,0 +1,11 @@
+Create a dic to store all found job details(location and title, when job was posted, and job link)
+Perform first filter on the dic by looking at when job was posted and only take recently posted jobs
+Iterate through filtered dic to look through all the links
+perform second filter on the links - filter based on keywords in job description, job requirements and salary ranges 
+return a clean dic of desired jobs to apply for
+
+
+
+other plans - get working with other jobs sites so you can post multiple site links in a at time and filter all (linkdin, indeed, glassdoor, etc)
+maybe make some sore of user interface to add experience
+
--- webscraper.py
+++ webscraper.py
@@ -14,41 +14,20 @@ try:
     # Decode the data (if it's in bytes) to a string
     html_content = data.decode('utf-8')
 
-    #remove HTML tags from page to clean it up :)
-    
-    # CLEANR = re.compile('<.*?>')
-    # clean_html_content = re.sub(CLEANR, '', html_content) 
-    # more_clean = clean_html_content.split()
-    # test_clean = html_content.split()
-    # f = open("test.txt", "w")  
-    # for word in test_clean:
-    #     for char in word:
-    #         f.write(char)
-    # f.close()
    
-   #Looks through the HTML decoded from the webpage for desired things only times posted right now but want to get link of job amd time
+   #Looks through the HTML decoded from the webpage for desired things using beautful soup
     soup = BeautifulSoup(html_content, 'html.parser')
     time_posted_list = soup.find_all("time")
+    
+    ## Creating times file to further clean it up
     # t = open("times.txt", "w")
     # for time in time_posted_list:
     #     time = str(time)
     #     time.split()
     #     t.write(time)
     # t.close()
-    
-    job_details_list = soup.find_all("span") #this will give basic info on the jobs - location and title - location can be used to filter out results
-    job_links_list = soup.find_all("a", class_="hidden-nested-link")
-    for link in job_links_list:
-        print(link['href'])  #This will print link of site of the job, not the job posting need to find the class of the job link
-    
 
-    job_links_list = soup.find_all("a", class_="base-card__full-linkabsolutetop-0right-0bottom-0left-0p-0z-[2]") #this is what I think the class should be
-    print(job_links_list)
-    for link in job_links_list:
-        print(link['href'])
-
-
-   #cleans up the output in another text file
+    ##cleans up the output in another text file
     # with open( 
     #     "times.txt", 'r') as r, open( 
     #         'times_clean.txt', 'w') as o: 
@@ -57,7 +36,16 @@ try:
     #         if line.strip(): 
     #             o.write(line) 
 
-    # f = open("times_clean.txt", "r") 
+    # f = open("times_clean.txt", "r")
+    
+    #this will give basic info on the jobs - location and title - location can be used to filter out results
+    job_details_list = soup.find_all("span") 
+    
+    #this gives the links of the jobs posted
+    job_links_list = soup.find_all(href=re.compile("/jobs/")) 
+    for link in job_links_list:
+        print(link['href'])
+
 
      
 except Exception as e:
